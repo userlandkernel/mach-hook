577a184683b13756399fe1031fdc5d47de07ceb6
Sem Voigtländer <43578852+userlandkernel@users.noreply.github.com>
2020-01-25 15:48:01 +0100

    Update hook.c

--- hook.c
+++ hook.c
@@ -1,6 +1,7 @@
 /*
  * Copyright 2020 (c) Sem Voigtländer
- */#include <stdio.h>
+ */
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
@@ -21,13 +22,7 @@
 
 #include <mach/mach.h>
 
-struct A64INSTr {
-    uint32_t *instructions;
-    uint64_t count;
-};
-
-extern kern_return_t mach_vm_protect(vm_map_t target_task, mach_vm_address_t address, mach_vm_size_t size, boolean_t set_maximum, vm_prot_t new_protection);
-extern kern_return_t mach_vm_read_overwrite(vm_map_t target_task, mach_vm_address_t address, mach_vm_size_t size, mach_vm_address_t data, mach_vm_size_t *outsize);
+#include "hook.h"
 
 uint64_t g_dyldSlide = 0;
 
